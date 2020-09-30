3f2401c835a2d8ee60867e4930ddf92b31170940
Kawan Vinicius Jesus de Carvalho <kawanvinicius14@gmail.com>
2020-09-29 21:08:27 -0300

    SWII6-TP02

---
+++ Controllers/BLsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TP02SWII;
+using TP02SWII.Models;
+
+namespace TP02SWII.Controllers
+{
+    public class BLsController : Controller
+    {
+        private readonly PortoContext _context;
+
+        public BLsController(PortoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BLs.ToListAsync());
+        }
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bL = await _context.BLs
+                .FirstOrDefaultAsync(m => m.BLId == id);
+            if (bL == null)
+            {
+                return NotFound();
+            }
+
+            return View(bL);
+        }
+
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BLId,Number,Consignee,Ship")] BL bL)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bL);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bL);
+        }
+
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bL = await _context.BLs.FindAsync(id);
+            if (bL == null)
+            {
+                return NotFound();
+            }
+            return View(bL);
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BLId,Number,Consignee,Ship")] BL bL)
+        {
+            if (id != bL.BLId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bL);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BLExists(bL.BLId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bL);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bL = await _context.BLs
+                .FirstOrDefaultAsync(m => m.BLId == id);
+            if (bL == null)
+            {
+                return NotFound();
+            }
+
+            return View(bL);
+        }
+
+   
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bL = await _context.BLs.FindAsync(id);
+            _context.BLs.Remove(bL);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BLExists(int id)
+        {
+            return _context.BLs.Any(e => e.BLId == id);
+        }
+    }
+}
